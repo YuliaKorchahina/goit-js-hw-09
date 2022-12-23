c6708cc9aad89a18d7f042e567a33df7a633a1ab
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-23 18:32:38 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+const handleStart = () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId !== true) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }
+};
+
+
+const handleStop = () => {
+  clearInterval(timerId);
+  if (timerId !== true) {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+};
+
+startBtn.addEventListener('click', handleStart);
+stopBtn.addEventListener('click', handleStop);
