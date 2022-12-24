bb8c281a58aee3a5df82371fed7a7af807a70f8c
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-24 21:42:12 +0200

    2 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const INVALID_MESSAGE_DATE = 'Please choose a date in the future';
+
+const inputEl = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+
+const dayEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let currentDateInCounter = {};
+btnStart.disabled = true;
+
+const getInvalidMessage = message => {
+  window.alert(message);
+};
+
+const isSelectedDateValid = (defaultDate, selectedDate) => {
+  return selectedDate < defaultDate;
+};
+
+const onCloseHandle = date => {
+  if (isSelectedDateValid(options.defaultDate, date)) {
+    getInvalidMessage(INVALID_MESSAGE_DATE);
+    return;
+  }
+  if (date) {
+    selectedDate = date;
+    btnStart.disabled = false;
+    currentDateInCounter = new Date(date - options.defaultDate);
+    const { day, hour, minute, second } = getDateElemnts(currentDateInCounter);
+    setDateToInput(day, hour, minute, second);
+  }
+};
+
+const defaultOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDate) {
+    onCloseHandle(selectedDate[0]);
+  },
+};
+
+const options = {
+  ...defaultOptions,
+};
+
+const setDateToInput = (day, hour, minute, second) => {
+  dayEl.textContent = day;
+  hoursEl.textContent = hour;
+  minutesEl.textContent = minute;
+  secondsEl.textContent = second;
+};
+
+const getDateElemnts = date => {
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return { day, hour, minute, second };
+};
+
+const handlCounter = () => {
+  if (currentDateInCounter <= 0) {
+    btnStart.disabled = true;
+    inputEl.disabled = true;
+    return;
+  }
+  currentDateInCounter -= 1000;
+  const { day, hour, minute, second } = getDateElemnts(
+    new Date(currentDateInCounter)
+  );
+  setDateToInput(day, hour, minute, second);
+};
+
+const handleStart = () => {
+  btnStart.disabled = true;
+  inputEl.disabled = true;
+  setInterval(() => {
+    handlCounter();
+  }, 1000);
+};
+
+flatpickr(inputEl, options);
+btnStart.addEventListener('click', handleStart);
